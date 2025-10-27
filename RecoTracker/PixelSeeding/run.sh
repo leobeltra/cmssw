1d74d6b809374185c57978b80330312da5769e2f
leonardo beltrame <leonardo.beltrame@cern.ch>
2025-10-27 16:23:32 +0100

    Fixed

--- RecoTracker/PixelSeeding/run.sh
+++ RecoTracker/PixelSeeding/run.sh
@@ -1,3 +1,14 @@
+LOCALPATH='/cms-hlt-nfs/data/relValTTbar_200PU/'
+echo "Input source: |${LOCALPATH}|"
+LOCALFILES=$(ls -1 ${LOCALPATH})
+ALL_FILES=""
+for f in ${LOCALFILES[@]}; do
+    ALL_FILES+="file:${LOCALPATH}/${f},"
+done
+# Remove the last character
+
+ALL_FILES="${ALL_FILES%?}"
+echo "Discovered files: $ALL_FILES"
 cmsDriver.py Phase2 -s L1P2GT,HLT:75e33_timing \
 --processName=HLTX \
 --conditions auto:phase2_realistic_T33 \
@@ -5,7 +16,7 @@ cmsDriver.py Phase2 -s L1P2GT,HLT:75e33_timing \
 --era Phase2C17I13M9 \
 --customise SLHCUpgradeSimulations/Configuration/aging.customise_aging_1000 \
 --eventcontent FEVTDEBUGHLT \
---filein=file:/data/lebeltra/AoS_benchmarks/input_root.root \
+--filein=$ALL_FILES \
 --mc \
 --inputCommands="keep *, drop *_hlt*_*_HLT, drop triggerTriggerFilterObjectWithRefs_l1t*_*_HLT" \
 -n -1 \
